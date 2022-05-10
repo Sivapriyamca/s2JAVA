f88bc22b1448189c403a3c1095c22949cb36d4db
Sivapriyamca <94525939+Sivapriyamca@users.noreply.github.com>
2022-05-10 16:29:41 +0530

    Add files via upload

---
+++ co4q1/graphics/triangle.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphics;
+
+/**
+ *
+ * @author sjcet
+ */
+import java.util.Scanner;
+public class triangle implements area_calc{
+    int side;
+   
+    public void area() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input side length of square : ");
+        side = sc.nextInt();
+        String area = Double.toString(side*side);
+        System.out.println("Area of the square : "+area);
+    }
+}
+ 
+    
